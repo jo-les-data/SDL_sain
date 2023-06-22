0047745cfcd4fd7273cdd011874ce6ed6335ec4b
jo-les-data <lavoine.joseph@yahoo.com>
2023-06-22 16:28:14 +0200

    correction colision

--- SDL_project3/interaction.c
+++ SDL_project3/interaction.c
@@ -18,13 +18,14 @@ void colorIntersection(SDL_Renderer* rend, SDL_Rect* rect1, SDL_Rect* rect2)
 
 void colision(Player* player1, Player* player2)
 {
-	SDL_bool hasIntersect = SDL_HasIntersection(player1->rect, player2->rect);
+	SDL_Rect intersect;
+	SDL_bool hasIntersect = SDL_HasIntersection(&(player1->rect), &(player2->rect), &intersect);
 	if (hasIntersect == SDL_TRUE)
 	{
 		float x_v = player1->x_vel;
 		float y_v = player1->y_vel;
 		player1->x_vel = player2->x_vel;
-		player2->y_vel = player2->y_vel;
+		player1->y_vel = player2->y_vel;
 		player2->x_vel = x_v;
 		player2->y_vel = y_v;
 	}
