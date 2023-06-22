49deeb5a85fcd0b4d0612c9033403537f9de7200
jo-les-data <lavoine.joseph@yahoo.com>
2023-06-22 17:00:40 +0200

    modification des colisions

--- SDL_project3/interaction.h
+++ SDL_project3/interaction.h
@@ -2,5 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include "player.h"
+
 void colorIntersection(SDL_Renderer* rend, SDL_Rect* rect1, SDL_Rect* rect2);
-void colision(Player* player1, Player* player2);
+void colision(Player* player, SDL_Rect* obstacle);
