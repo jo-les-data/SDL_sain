ff0244883fcf65c417818f81cb33d38fe09a8cfc
jo-les-data <lavoine.joseph@yahoo.com>
2023-06-23 10:00:24 +0200

    fin des modification sur release.h et .c

---
+++ SDL_project3/release.c
@@ -0,0 +1,22 @@
+#include <SDL.h>
+#include "player.h"
+
+
+// Liberation des ressources SDL
+void ressourceRelease(SDL_Renderer * renderer, SDL_Window * window, 
+    SDL_Surface * image, 
+    SDL_Surface * imagebu, SDL_Surface * imagerab, 
+    SDL_Texture * texture, SDL_Texture * texturebu,
+    SDL_Texture * texturerab)
+{
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
+    SDL_DestroyTexture(texturebu);
+    SDL_FreeSurface(imagebu);
+    SDL_DestroyTexture(texturerab);
+    SDL_FreeSurface(imagerab);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
--- SDL_project3/release.h
+++ SDL_project3/release.h
@@ -1,3 +1,7 @@
 #pragma once
 #include <SDL.h>
-void ressourceRelease(SDL_Renderer* renderer, SDL_Window* window);
+void ressourceRelease(SDL_Renderer* renderer, SDL_Window* window,
+    SDL_Surface* image,
+    SDL_Surface* imagebu, SDL_Surface* imagerab,
+    SDL_Texture* texture, SDL_Texture* texturebu,
+    SDL_Texture* texturerab);
