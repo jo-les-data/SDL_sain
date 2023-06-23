01ab71c64b061aa101a957d545317904b1caa74f
Vincent <flauss.vincent@gmail.com>
2023-06-23 09:31:58 +0200

    remaniement des commentaires

--- SDL_project3/update.c
+++ SDL_project3/update.c
@@ -29,7 +29,7 @@ void update(bool running, Player* p1, Player* p2, SDL_Renderer* rend, SDL_Textur
                 break;
 
 
-                // Changement de l'état des boutons quand les touches sont appuyées
+                // Changement de l etat des boutons quand les touches sont appuyées
             case SDL_KEYDOWN:
                 switch (event.key.keysym.scancode)
                 {
@@ -68,7 +68,7 @@ void update(bool running, Player* p1, Player* p2, SDL_Renderer* rend, SDL_Textur
                 break;
 
 
-                //Changement de l'état des boutons quand les touches sont enlevés
+                //Changement de l etat des boutons quand les touches sont enlevés
             case SDL_KEYUP:
                 switch (event.key.keysym.scancode)
                 {
